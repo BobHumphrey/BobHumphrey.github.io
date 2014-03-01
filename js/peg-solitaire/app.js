f55d1566c65c19f437d7e4bc0b44f3f6dfde8029
Bob Humphrey <bobhumphrey@gmail.com>
2014-03-01 11:16:04 -0500

    Add inspiration section.  Misc. updates.

--- js/peg-solitaire/app.js
+++
@@ -1,8 +0,0 @@
-var pegApp = angular.module('pegApp', [], function() {
-});
-
-pegApp.config([
-  '$interpolateProvider', function($interpolateProvider) {
-    return $interpolateProvider.startSymbol('{(').endSymbol(')}');
-  }
-]);
